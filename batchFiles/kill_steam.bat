2aa268b0e440a29c3bbc4d8c70f476d694d62198
Marvin Bäumer <m.baeumee.industries@gmail.com>
2025-04-28 16:36:48 +0200

    update: change audio output

--- batchFiles/kill_steam.bat
+++ batchFiles/kill_steam.bat
@@ -1,2 +1,4 @@
+@echo off
 taskkill /f /im steam.exe
+"C:\ToolsNirSoft\nircmd.exe" setdefaultsounddevice "Headphones"
 "C:\ToolsNirSoft\nircmd.exe" setprimarydisplay 1
--- batchFiles/open_steam.bat
+++ batchFiles/open_steam.bat
@@ -2,5 +2,6 @@
 taskkill /f /im steam.exe
 timeout /t 1
 start "" "C:\Program Files (x86)\Steam\steam.exe" -bigpicture -tenfoot
+"C:\ToolsNirSoft\nircmd.exe" setdefaultsounddevice "PHL 323E7"
 timeout /t 5
 "C:\ToolsNirSoft\nircmd.exe" win move ititle "Steam Big Picture" 4480 0 1920 1080
